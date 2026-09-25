32d928ac538388aaedf3fd9bdc2ef8d3927b54cf
GauravSharma82715 <gau159157@gmail.com>
2026-09-25 18:03:47 +0530

    some changes

--- server/config/nodeMailer.ts
+++ server/config/nodeMailer.ts
@@ -6,7 +6,7 @@ const transporter = createTransport({
     port: 587,
 
     auth: {
-        user: process.env.SMTP_USER,
+        user: process.env.SMTP_USER || process.env.SMTP_EMAIL,
         pass: process.env.SMTP_PASS,
     },
 });
